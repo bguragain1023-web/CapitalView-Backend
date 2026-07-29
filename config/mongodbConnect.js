720db0e35692f55448b5e68bce7fede1a242e3dc
Brazesh Guragain <b.guragain1023@gmail.com>
2026-07-30 08:35:37 +1000

    ready for production

--- config/mongodbConnect.js
+++ config/mongodbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const MONGO_URL = "mongodb://localhost:27017/capitalView";
+const MONGO_URL = process.env.MONGO_URL;
 
 export const connectDB = async () => {
   try {
